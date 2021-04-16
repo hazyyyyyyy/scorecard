03197ce90e351d97444da1492dfb1d04580686e2
hazyyyyyyy <935768177@qq.com>
2021-04-16 17:02:02 +0800

    class

--- scorecard_class.py
+++ scorecard_class.py
@@ -13,8 +13,10 @@ class ScoreCard:
         self.orig_x = orig_x
         self.orig_y = orig_y
     def transform_discrete(self): #按照transform rule的bad_rate转换类别型变量为数值
-        x = self.orig_x
-        y = self.orig_y
+        x = self.orig_x.copy()
+        y = self.orig_y.copy()
+        x.reset_index(drop=True, inplace=True)
+        y.reset_index(drop=True, inplace=True)
         x_types = pd.Series(x.dtypes, dtype='str')
         
         x_num_position = np.where( (x_types=='float64')|(x_types=='int64') )[0]
@@ -28,9 +30,26 @@ class ScoreCard:
         x_category_transfer_rule = list(np.zeros([len(x_category.columns)])) 
         # 对应的变量名称
         x_category_transfer_rule_featurename = pd.Series(list(range(0,len(x_category.columns))), index=x_category.columns)
-        #      
+        # 类别变量转化后的值
+        x_category_transform = pd.DataFrame(np.zeros(x_category.shape), columns=x_category.columns)
         
-        
+        for i in range(0, x_category.shape[1]):
+            unique_value = np.unique(x_category.iloc[:,i])
+            unique_value = unique_value[np.where(unique_value!='blank')]
+            this_transform_rule = pd.concat([pd.DataFrame(unique_value,columns=['raw_data']),\
+                                            pd.DataFrame(np.zeros([len(unique_value),2]), columns=['transform_data', 'bad_rate'])])
+            
+            for j in range(0, len(unique_value)):
+                this_unique = unique_value[j]
+                this_bad_num = len(np.where( (y==1)&(x_category.iloc[:,i]==this_unique) )[0])
+                this_good_num = len(np.where( (y==0)&(x_category.iloc[:,i]==this_unique) )[0])
+                # 防止good_num为零报错
+                if this_good_num==0:
+                    this_good_num=0.5
+                this_transform_rule.iloc[j,2] = this_bad_num/this_good_num
+            
+            this_transform_rule = this_transform_rule.sort_values(by='bad_rate')
+            this_transform_rule.iloc[:,1] = list(range(len(unique_value),0,-1))
         
     
 
